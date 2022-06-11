87576652298ed18443c29f8a058a1ad9d1b54f79
Hunter <hunterl3535@yahoo.com>
2022-06-11 12:04:11 -0500

    finished juicebox part 2 part 3

--- api/tags.js
+++ api/tags.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const tagsRouter = express.Router();
-const { getAllTags } = require('../db');
+const { getAllTags, getPostsByTagName } = require('../db');
 
 tagsRouter.get('/', async (req, res) => {
   const allTags = await getAllTags()
@@ -9,5 +9,48 @@ tagsRouter.get('/', async (req, res) => {
     allTags
   });
 })
+
+tagsRouter.get('/:tagName/posts', async (req, res, next) => {
+  // read the tagname from the params
+  const { tagName } = req.params;
+
+  try {
+      
+      // use our method to get posts by tag name from the db
+      const postList = await getPostsByTagName(tagName);
+      // send out an object to the client { posts: // the posts }
+      
+      const filteredPosts = postList.filter(post => {
+
+          if (post.active) {
+              return true;
+          }
+
+          if (req.user && post.author.id === req.user.id) {
+              return true;
+          }
+
+          return false;
+
+      });
+
+      if (filteredPosts) {
+          console.log(filteredPosts);
+
+          res.send({ posts: filteredPosts })
+      
+      } else {
+          next({
+              name: 'ErrorGettingPostByTag',
+              message: 'Cannot get posts by tag name'
+          })
+      }
+  }
+  catch ({ name, message }) {
+      // forward the name and message to the error handler
+      next({ name, message });
+
+  }
+});
 
 module.exports = tagsRouter;
--- db/index.js
+++ db/index.js
@@ -98,7 +98,7 @@ async function createPost({
   authorId,
   title,
   content,
-  tags = [] // this is new
+  tags = []
 }) {
   try {
     const { rows: [ post ] } = await client.query(`
@@ -292,6 +292,13 @@ async function getPostById(postId) {
       WHERE id=$1;
     `, [postId]);
 
+    if (!post) {
+      throw {
+        name: "PostNotFoundError",
+        message: "Could not find a post with that postId"
+      };
+    }
+
     const { rows: tags } = await client.query(`
       SELECT tags.*
       FROM tags
@@ -330,5 +337,6 @@ module.exports = {
   addTagsToPost,
   getPostsByTagName,
   getAllTags,
-  getUserByUsername
+  getUserByUsername,
+  getPostById
 }
